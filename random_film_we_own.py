bb56f8d3a524d7f194c355977714ea50759d984c
Alex <guitarist_of_ra@pacbell.net>
2020-04-08 01:50:47 -0600

    added ability to write out files and read them in later, making later runs much faster

--- random_film_we_own.py
+++ random_film_we_own.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import requests
+from pathlib import Path
 import random
 
 ##################################
@@ -106,28 +107,34 @@ for i in range(len(keyword)):
 	# allnew
 	elif keyword[i] == 'allnew' and equals[i] == '=':
 		allnew = int(parameter[i])
+	# newgenres
+	elif keyword[i] == 'newgenres' and equals[i] == '=':
+		newgenres = int(parameter[i])
 # Defaults, if parameters not found:
-if not('minyear' in locals()):
+if 'minyear' not in locals():
 	print('\nminyear not found in input file; minyear = 0 by default.')
 	minyear = 0
-if not('maxyear' in locals()):
+if 'maxyear' not in locals():
 	print('\nmaxyear not found in input file; maxyear = 0 by default.')
 	maxyear = 0
-if not('minrating' in locals()):
+if 'minrating' not in locals():
 	print('\nminrating not found in input file; minrating = 0 by default.')
 	minrating = 0
-if not('maxrating' in locals()):
+if 'maxrating' not in locals():
 	print('\nmaxrating not found in input file; maxrating = 0 by default.')
 	maxrating = 0
-if not('dounrated' in locals()):
+if 'dounrated' not in locals():
 	print('\ndounrated not found in input file; dounrated = 0 by default.')
 	dounrated = 0
-if not('genre' in locals()):
+if 'genre' not in locals():
 	print('\ngenre not found in input file; genre = any by default.')
 	genre = 'any'
-if not('allnew' in locals()):
+if 'allnew' not in locals():
 	print('\nallnew not found in input file; allnew = 0 by default.')
 	allnew = 0
+if 'newgenres' not in locals():
+	print('\nnewgenres not found in input file; newgenres = 0 by default.')
+	newgenres = 0
 
 # Check inputs:
 if dounrated == 1 and minrating != 0:
@@ -138,96 +145,177 @@ elif dounrated == 1 and maxrating != 0:
 # Status update:
 print('\nReading in Amanda\'s and Alex\'s film collection.')
 
-# The base url of our film collection:
-url = 'https://letterboxd.com/moogic/list/moogics-dvd-collection/detail/'
-# Grab source code for the first page:
-r = requests.get(url)
-source = r.text
+# If allnew = 0, check for previous film collection output:
+collectionflag = 0
+if allnew == 0:
+	collectionpath = Path('Data/Collection.csv')
+	if collectionpath.exists():
+		# If there is previous output, read it in:
+		collectionflag = 1
+		films = []
+		years = []
+		with open('Data/Collection.csv') as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+				films = films+[row[0]]
+				years = years+[row[1]]
+	# Make year values integers:
+	years = [int(item) for item in years]
+# Otherwise, grab info from internet:
+if collectionflag == 0:
+	# The base url of our film collection:
+	url = 'https://letterboxd.com/moogic/list/moogics-dvd-collection/detail/'
+	# Grab source code for the first page:
+	r = requests.get(url)
+	source = r.text
+	# Find the number of pages
+	pages = int(getstrings('last','/moogic/list/moogics-dvd-collection/detail/page/','/">',source))
+	# Loop through all pages and grab all the film titles:
+	pageflag = 0
+	# Initialize results:
+	films = []
+	years = []
+	# Start on page 1, get the films and years:
+	films = films+getstrings('all','data-film-slug="/film/','/"',source)
+	years = years+getstrings('all','/films/year/','/">',source)
+	# Now loop through the rest of the pages:
+	if pageflag == 0:
+		for page in range(pages-1):
+			# Start on page 2:
+			page = str(page + 2)
+			# Grab source code of the page:
+			r = requests.get(url+'page/'+page+'/')
+			source = r.text
+			# Get films and years:
+			films = films+getstrings('all','data-film-slug="/film/','/"',source)
+			years = years+getstrings('all','/films/year/','/">',source)
+	# Make year values integers:
+	years = [int(item) for item in years]
+	# Make sure the lengths match:
+	if len(films) != len(years):
+		sys.exit('ERROR - in function "MAIN" - Number of films does not match number of ratings')
+	# Write out the data:
+	with open('Data/Collection.csv', mode='w') as outfile:
+		csvwriter = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		for i in range(len(films)):
+			csvwriter.writerow([films[i],years[i]])
 
-# Find the number of pages
-pages = int(getstrings('last','/moogic/list/moogics-dvd-collection/detail/page/','/">',source))
+# Status update:
+print('\nGetting genres for all collection films.')
 
-# Loop through all pages and grab all the film titles:
-pageflag = 0
-# Initialize results:
-films = []
-years = []
-# Start on page 1, get the films and years:
-films = films+getstrings('all','data-film-slug="/film/','/"',source)
-years = years+getstrings('all','/films/year/','/">',source)
-# Now loop through the rest of the pages:
-if pageflag == 0:
+# If newgenres = 0, check for previous film genre output:
+if newgenres == 0:
+	genrepath = Path('Data/Genres.csv')
+	if genrepath.exists():
+		# If there is previous output, read it in:
+		films2 = []
+		years2 = []
+		genres2 = []
+		with open('Data/Genres.csv') as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+				films2 = films2+[row[0]]
+				years2 = years2+[row[1]]
+				genres2 = genres2+[row[2]]
+# Go through all collection films, and either
+# grab info from internet, or use previous info if available:
+films3 = []
+years3 = []
+genres3 = []
+for i in range(len(films)):
+	# Check if previous info available:
+	genreflag = 0
+	if newgenres == 0:
+		if films[i] in films2:
+			genreflag = 1
+			films3 = films3+[films[i]]
+			years3 = years3+[years[i]]
+			genres3 = genres3+[genres2[films2.index(films[i])]]
+	# If previous info not available, get it from the internet:
+	if genreflag == 0:
+		url = 'https://letterboxd.com/film/'+films[i]+'/genres/'
+		# Grab source code for genre page:
+		r = requests.get(url)
+		source = r.text
+		# Find the genres:
+		genres = getstrings('all','"/films/genre/','/"',source)
+		flag = 0
+		genrestring = ''
+		for j in range(len(genres)):
+			genrestring = genrestring+genres[j]
+			if j < len(genres)-1:
+				genrestring = genrestring+' '
+		films3 = films3+[films[i]]
+		years3 = years3+[years[i]]
+		genres3 = genres3+[genrestring]
+# Make sure the lengths match:
+if len(films3) != len(genres3):
+	sys.exit('ERROR - in function "MAIN" - Number of films does not match number of genres')
+# Write out the data:
+if newgenres == 1:
+	with open('Data/Genres.csv', mode='w') as outfile:
+		csvwriter = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		for i in range(len(films3)):
+			csvwriter.writerow([films3[i],years3[i],genres3[i]])
+
+# Status update:
+print('\nGetting all of Alex\'s film ratings.')
+
+# If allnew = 0, check for previous film ratings output:
+ratingsflag = 0
+if allnew == 0:
+	ratingspath = Path('Data/Ratings.csv')
+	if ratingspath.exists():
+		# If there is previous output, read it in:
+		ratingsflag = 1
+		films4 = []
+		ratings4 = []
+		with open('Data/Ratings.csv') as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+				films4 = films4+[row[0]]
+				ratings4 = ratings4+[row[1]]
+	# Make ratings values integers:
+	ratings4 = [int(item) for item in ratings4]
+# Otherwise, grab info from internet:
+if ratingsflag == 0:
+	# Grab all film ratings:
+	# The base url of the user's film ratings:
+	url = 'https://letterboxd.com/moogic/films/ratings/'
+	# Grab source code for first ratings page:
+	r = requests.get(url)
+	source = r.text
+	# Find the number of ratings pages:
+	pages = int(getstrings('last','/moogic/films/ratings/page/','/"',source))
+	# Loop through all pages and grab all the film titles:
+	# Initialize results:
+	films4 = []
+	ratings4 = []
+	# Start on page 1, get the films:
+	films4 = films4+getstrings('all','data-film-slug="/film/','/"',source)
+	# Do the same for ratings:
+	ratings4 = ratings4+getstrings('all','rating rated-','">',source)
+	# Now loop through the rest of the pages:
 	for page in range(pages-1):
 		# Start on page 2:
 		page = str(page + 2)
 		# Grab source code of the page:
 		r = requests.get(url+'page/'+page+'/')
 		source = r.text
-		# Get films and years:
-		films = films+getstrings('all','data-film-slug="/film/','/"',source)
-		years = years+getstrings('all','/films/year/','/">',source)
-# Make year values integers:
-years = [int(item) for item in years]
-
-# Make sure the lengths match:
-if len(films) != len(years):
-	sys.exit('ERROR - in function "MAIN" - Number of films does not match number of ratings')
-
-# Status update:
-print('\nLimiting years, if requested.')
-
-# Limit years:
-cutfilms = []
-cutyears = []
-if minyear != 0 or maxyear != 0:
-	if maxyear == 0:
-		maxyear = 3000
-	for i in range(len(films)):
-		if years[i] >= minyear and years[i] <= maxyear:
-			cutfilms = cutfilms+[films[i]]
-			cutyears = cutyears+[years[i]]
-else:
-	cutfilms = [item for item in films]
-	cutyears = [item for item in years]
-if len(cutfilms) == 0:
-	sys.exit('ERROR - in function "MAIN" - No films in the year range given')
-
-# Status update:
-print('\nGetting all of Alex\'s film ratings.')
-
-# Grab all film ratings:
-# The base url of the user's film ratings:
-url2 = 'https://letterboxd.com/moogic/films/ratings/'
-# Grab source code for first ratings page:
-r2 = requests.get(url2)
-source2 = r2.text
-# Find the number of ratings pages:
-pages2 = int(getstrings('last','/moogic/films/ratings/page/','/"',source2))
-# Loop through all pages and grab all the film titles:
-# Initialize results:
-films2 = []
-ratings2 = []
-# Start on page 1, get the films:
-films2 = films2+getstrings('all','data-film-slug="/film/','/"',source2)
-# Do the same for ratings:
-ratings2 = ratings2+getstrings('all','rating rated-','">',source2)
-# Now loop through the rest of the pages:
-for page in range(pages2-1):
-	# Start on page 2:
-	page = str(page + 2)
-	# Grab source code of the page:
-	r2 = requests.get(url2+'page/'+page+'/')
-	source2 = r2.text
-	# Get films:
-	films2 = films2+getstrings('all','data-film-slug="/film/','/"',source2)
-	# Get ratings:
-	ratings2 = ratings2+getstrings('all','rating rated-','">',source2)
-# Make ratings values integers:
-ratings2 = [int(item) for item in ratings2]
-
-# Make sure the lengths match:
-if len(films2) != len(ratings2):
-	sys.exit('ERROR - in function "MAIN" - Number of films does not match number of ratings')
+		# Get films:
+		films4 = films4+getstrings('all','data-film-slug="/film/','/"',source)
+		# Get ratings:
+		ratings4 = ratings4+getstrings('all','rating rated-','">',source)
+	# Make ratings values integers:
+	ratings4 = [int(item) for item in ratings4]
+	# Make sure the lengths match:
+	if len(films4) != len(ratings4):
+		sys.exit('ERROR - in function "MAIN" - Number of films does not match number of ratings')
+	# Write out the data:
+	with open('Data/Ratings.csv', mode='w') as outfile:
+		csvwriter = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		for i in range(len(films4)):
+			csvwriter.writerow([films4[i],ratings4[i]])
 
 # Status update:
 print('\nMatching film collection with ratings.')
@@ -235,129 +323,107 @@ print('\nMatching film collection with ratings.')
 # Match ratings if they exist, otherwise make them -1.0:
 newfilms = []
 newyears = []
+newgenres = []
 newratings = []
-for i in range(len(cutfilms)):
+for i in range(len(films3)):
 	flag = 0
 	j = 0
-	while flag == 0 and j < len(films2):
-		if cutfilms[i] == films2[j]:
+	while flag == 0 and j < len(films4):
+		if films3[i] == films4[j]:
 			flag = 1
-			newfilms = newfilms+[cutfilms[i]]
-			newyears = newyears+[cutyears[i]]
-			newratings = newratings+[ratings2[j]/2.0]
-			del films2[j]
-			del ratings2[j]
+			newfilms = newfilms+[films3[i]]
+			newyears = newyears+[years3[i]]
+			newgenres = newgenres+[genres3[i]]
+			newratings = newratings+[ratings4[j]/2.0]
+			del films4[j]
+			del ratings4[j]
 		j = j+1
 	if flag == 0:
-		newfilms = newfilms+[cutfilms[i]]
-		newyears = newyears+[cutyears[i]]
+		newfilms = newfilms+[films3[i]]
+		newyears = newyears+[years3[i]]
+		newgenres = newgenres+[genres3[i]]
 		newratings = newratings+[-1.0]
+
+# Status update:
+print('\nLimiting years, if requested.')
+
+# Limit years:
+ycutfilms = []
+ycutyears = []
+ycutgenres = []
+ycutratings = []
+if minyear != 0 or maxyear != 0:
+	if maxyear == 0:
+		maxyear = 3000
+	for i in range(len(newfilms)):
+		if newyears[i] >= minyear and newyears[i] <= maxyear:
+			ycutfilms = ycutfilms+[newfilms[i]]
+			ycutyears = ycutyears+[newyears[i]]
+			ycutgenres = ycutgenres+[newgenres[i]]
+			ycutratings = ycutratings+[newratings[i]]
+else:
+	ycutfilms = [item for item in newfilms]
+	ycutyears = [item for item in newyears]
+	ycutgenres = [item for item in newgenres]
+	ycutratings = [item for item in newratings]
+if len(ycutfilms) == 0:
+	sys.exit('ERROR - in function "MAIN" - No films in the year range given')
 
 # Status update:
 print('\nLimiting ratings, if requested.')
 
-# Limit ratings?
-finalfilms = []
-finalyears = []
-finalratings = []
+# Limit ratings:
+rcutfilms = []
+rcutyears = []
+rcutgenres = []
+rcutratings = []
 if minrating != 0 or maxrating != 0:
-	for i in range(len(newfilms)):
-		if newratings[i] >= minrating and newratings[i] <= maxrating:
-			finalfilms = finalfilms+[newfilms[i]]
-			finalyears = finalyears+[newyears[i]]
-			finalratings = finalratings+[newratings[i]]
-	if len(finalfilms) == 0:
+	for i in range(len(ycutfilms)):
+		if ycutratings[i] >= minrating and ycutratings[i] <= maxrating:
+			rcutfilms = rcutfilms+[ycutfilms[i]]
+			rcutyears = rcutyears+[ycutyears[i]]
+			rcutgenres = rcutgenres+[ycutgenres[i]]
+			rcutratings = rcutratings+[ycutratings[i]]
+	if len(rcutfilms) == 0:
 		sys.exit('ERROR - in function "MAIN" - No films in the rating range given')
 # Otherwise, unrated films only?:
 elif dounrated == 1:
-	for i in range(len(newfilms)):
-		if newratings[i] == -1:
-			finalfilms = finalfilms+[newfilms[i]]
-			finalyears = finalyears+[newyears[i]]
-			finalratings = finalratings+[newratings[i]]
+	for i in range(len(ycutfilms)):
+		if ycutratings[i] == -1:
+			rcutfilms = rcutfilms+[ycutfilms[i]]
+			rcutyears = rcutyears+[ycutyears[i]]
+			rcutgenres = rcutgenres+[ycutgenres[i]]
+			rcutratings = rcutratings+[ycutratings[i]]
+	if len(rcutfilms) == 0:
+		sys.exit('ERROR - in function "MAIN" - No films are unrated (WOW!)')
 # Otherwise, just keep all of the films:
 else:
-	finalfilms = [item for item in newfilms]
-	finalyears = [item for item in newyears]
-	finalratings = [item for item in newratings]
+	rcutfilms = [item for item in ycutfilms]
+	rcutyears = [item for item in ycutyears]
+	rcutgenres = [item for item in ycutgenres]
+	rcutratings = [item for item in ycutratings]
 
-# Status update:
-print('\nGetting genres for all remaining films.')
-
-# Grab genres for every remaining film and check them:
-finalfilms2 = []
-finalyears2 = []
-finalratings2 = []
-finalgenres2 = []
-for i in range(len(finalfilms)):
-	url3 = 'https://letterboxd.com/film/'+finalfilms[i]+'/genres/'
-	# Grab source code for genre page:
-	r3 = requests.get(url3)
-	source3 = r3.text
-	# Find the genres:
-	genres3 = getstrings('all','"/films/genre/','/"',source3)
-	# If genre requested:
-	if genre != 'any':
-		if genre == 'romcom':
-			romflag = 0
-			comflag = 0
-			genrestring = ''
-			for j in range(len(genres3)):
-				genrestring = genrestring+genres3[j]
-				if j < len(genres3)-1:
-					genrestring = genrestring+', '
-				if genres3[j] == 'romance':
-					romflag = 1
-				elif genres3[j] == 'comedy':
-					comflag = 1
-				if romflag == 1 and comflag == 1 and j == len(genres3)-1:
-					finalfilms2 = finalfilms2+[finalfilms[i]]
-					finalyears2 = finalyears2+[finalyears[i]]
-					finalratings2 = finalratings2+[finalratings[i]]
-					finalgenres2 = finalgenres2+[genrestring]
-		else:
-			flag = 0
-			genrestring = ''
-			for j in range(len(genres3)):
-				genrestring = genrestring+genres3[j]
-				if j < len(genres3)-1:
-					genrestring = genrestring+', '
-				if genres3[j] == genre:
-					flag = 1
-				if flag == 1 and j == len(genres3)-1:
-					finalfilms2 = finalfilms2+[finalfilms[i]]
-					finalyears2 = finalyears2+[finalyears[i]]
-					finalratings2 = finalratings2+[finalratings[i]]
-					finalgenres2 = finalgenres2+[genrestring]
-		if len(finalfilms2) == 0:
-			sys.exit('ERROR - in function "MAIN" - No films match the genre given')
-	# Otherwise just record them all together (if there's more than one):
-	else:
-		genrestring = ''
-		for j in range(len(genres3)):
-			genrestring = genrestring+genres3[j]
-			if j < len(genres3)-1:
-				genrestring = genrestring+', '
-		finalfilms2 = finalfilms2+[finalfilms[i]]
-		finalyears2 = finalyears2+[finalyears[i]]
-		finalratings2 = finalratings2+[finalratings[i]]
-		finalgenres2 = finalgenres2+[genrestring]
+# Denote the final result arrays:
+finalfilms = [item for item in rcutfilms]
+finalyears = [item for item in rcutyears]
+finalgenres = [item for item in rcutgenres]
+finalratings = [item for item in rcutratings]
 
 # Status update:
 print('\nRequested films obtained. Choosing one randomly.')
 
 # Grab a random film:
 random.seed()
-choice = random.choice(finalfilms2)
+choice = random.choice(finalfilms)
 # Get the year and rating:
-year = finalyears2[finalfilms2.index(choice)]
-rating = finalratings2[finalfilms2.index(choice)]
-xgenre = finalgenres2[finalfilms2.index(choice)]
+year = finalyears[finalfilms.index(choice)]
+genre = finalgenres[finalfilms.index(choice)]
+rating = finalratings[finalfilms.index(choice)]
 
 # Print out the result:
 print('\n********************')
 if rating == -1.0:
-	print('{} -- {:d} -- no rating -- {}'.format(choice,year,xgenre))
+	print('{} -- {:d} -- no rating -- {}'.format(choice,year,genre))
 else:
-	print('{} -- {:d} -- {:.1f}/5 -- {}'.format(choice,year,rating,xgenre))
+	print('{} -- {:d} -- {:.1f}/5 -- {}'.format(choice,year,rating,genre))
 print('********************\n')
