af4e50dce36442e3dee6e84d4f76e0866500ff96
Alex Spacek <aespacek@gmail.com>
2025-02-16 10:22:43 -0800

    Updated and finished Collection_actors_check.py routine.

--- Collection_actors_check.py
+++ Collection_actors_check.py
@@ -43,9 +43,12 @@ from Getfilminfo import getfilminfo
 
 film1 = input('\nEnter Letterboxd film 1: ')
 
+outputfile = open('C_a_c_Output_'+film1+'.txt','w')
+
 # Compare with every film in our collection
 # Status update:
 print('\nReading in Amanda\'s and Alex\'s film collection.')
+outputfile.write('\nReading in Amanda\'s and Alex\'s film collection.')
 
 allnew = 1
 # If allnew = 0, check for previous film collection output:
@@ -107,44 +110,84 @@ if collectionflag == 0:
 
 # Status update:
 print('\nNumber of films in collection: '+str(len(films)))
+outputfile.write('\nNumber of films in collection: '+str(len(films)))
+
+# Read in actors and films to ignore
+ignorefilms = []
+ignoreactors = []
+with open('C_a_c_films_to_ignore.txt') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter='\n')
+	for row in csv_reader:
+		ignorefilms = ignorefilms+[row[0]]
+with open('C_a_c_actors_to_ignore.txt') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter='\n')
+	for row in csv_reader:
+		ignoreactors = ignoreactors+[row[0]]
+
 print('\nChecking all owned films that share at least one actor with '+film1)
+outputfile.write('\n\nChecking all owned films that share at least one actor with '+film1)
 
 allfilms = []
 allactors = []
-numactors = []
+n = 0
 
 for eachfilm in films:
-	if film1 != eachfilm:
+	n = n+1
+	print('\n'+str(n)+' / '+str(len(films))+' Checking '+eachfilm)
+	outputfile.write('\n\n'+str(n)+' / '+str(len(films))+' Checking '+eachfilm)
+	skipflag1 = 0
+	for skipfilm in ignorefilms:
+		if eachfilm == skipfilm:
+			skipflag1 = 1
+			print('Skipping '+eachfilm+' due to film Sheet match')
+			outputfile.write('\nSkipping '+eachfilm+' due to film Sheet match')
+	if film1 != eachfilm and skipflag1 == 0:
 		filmcomparison = [film1,eachfilm]
 		ratings = [0,0]
 		filmcomparison,ratings,actors = getfilminfo(filmcomparison,ratings,['actors'])
-		
-		alreadydone = []
+		actors1 = []
+		actors2 = []
+		for i in range(len(filmcomparison)):
+			if filmcomparison[i] == film1:
+				actors1 = actors1+[actors[i]]
+			else:
+				actors2 = actors2+[actors[i]]
 		matchflag = 0
-		for i in range(len(actors)):
-			for j in range(len(actors)):
-				if i != j:
-					if actors[i] == actors[j]:
-						flag = 0
-						for k in range(len(alreadydone)):
-							if i == alreadydone[k]:
-								flag = 1
-						if flag == 0:
-							allactors = allactors+[actors[i]]
-							if matchflag == 0:
-								numactors = numactors+[1]
-							else:
-								numactors[-1] = numactors[-1]+1
-							alreadydone = alreadydone+[j]
-							matchflag = 1
-		if matchflag == 1:
-			allfilms = allfilms+[eachfilm]
+		for i in range(len(actors2)):
+			skipflag2 = 0
+			for skipactor in ignoreactors:
+				if actors2[i] == skipactor:
+					skipflag2 = 1
+					print('Skipping '+actors2[i]+' due to actor Sheet match')
+					outputfile.write('\nSkipping '+actors2[i]+' due to actor Sheet match')
+			if skipflag2 == 0:
+				for j in range(len(actors1)):
+					if actors2[i] == actors1[j]:
+						skipflag3 = 0
+						for k in range(len(actors2)):
+							for skipactor in ignoreactors:
+								if actors2[k] == skipactor:
+									for l in range(len(actors1)):
+										if l != j and actors2[k] == actors1[l]:
+											skipflag3 = 1
+											skippedactor = actors2[k]
+						if skipflag3 == 0:
+							allfilms = allfilms+[eachfilm]
+							allactors = allactors+[actors2[i]]
+						else:
+							print(actors2[i]+' is in '+eachfilm+' but skipping due to '+skippedactor+' being in it')
+							outputfile.write('\n'+actors2[i]+' is in '+eachfilm+' but skipping due to '+skippedactor+' being in it')
 
-actorcount = 0
-for i in range(len(allfilms)):
+print('\n**************************************************')
+outputfile.write('\n\n**************************************************')
+
+sortedactors,sortedfilms = numsort(allactors,allfilms,1,0)
+
+for i in range(len(sortedactors)):
 	print('')
-	print(allfilms[i])
-	for j in range(numactors[i]):
-		print('\t'+allactors[actorcount])
-		actorcount = actorcount+1
+	print(sortedactors[i]+' - '+sortedfilms[i])
+	outputfile.write('\n\n'+sortedactors[i]+' - '+sortedfilms[i])
 print('')
+outputfile.write('\n')
+
+outputfile.close()
